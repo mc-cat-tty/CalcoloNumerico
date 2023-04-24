03e570b6b14f3876049d9c62c942fa6caa3deb52
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2023-04-24 12:36:56 +0200

    upload iterative methods MatLab's implementation

---
+++ Esercizi/iterative_gauss_sidel.m
@@ -0,0 +1,21 @@
+function [x_next, k] = iterative_gauss_sidel(x_corr, A, b, K_max, tau)
+% x_corr (ovvero x0) random
+% K_max intorno a 10^3
+% tau intorno a 10^-2
+D = diag(diag(A));
+E = -1 .* tril(A, -1);
+F = -1 .* triu(A, 1);
+D_E = D - E;
+norm_tau_b = tau * norm(b);
+
+for k = 1:K_max
+    p = F * x_corr + b;
+    x_next = D_E\p;
+    res = b - A * x_corr;
+    if (norm(x_next - x_corr) < tau * norm(x_next) && norm(res) < norm_tau_b)
+        return;
+    end
+    x_corr = x_next;
+end
+
+error("Safeguard parameter triggered");
---
+++ Esercizi/iterative_jacobi.m
@@ -0,0 +1,19 @@
+function [x_next, k] = iterative_jacobi(x_corr, A, b, K_max, tau)
+% x_corr (ovvero x0) random
+% K_max intorno a 10^3
+% tau intorno a 10^-2
+D = diag(diag(A));
+E = -1 .* tril(A, -1);
+F = -1 .* triu(A, 1);
+norm_tau_b = tau * norm(b);
+
+for k = 1:K_max
+x_next = diag_solver(D, (E+F) * x_corr + b);
+res = b - A * x_corr;
+if (norm(x_next - x_corr) < tau * norm(x_next) && norm(res) < norm_tau_b)
+    return;
+end
+x_corr = x_next;
+end
+
+error("Safeguard parameter triggered");
